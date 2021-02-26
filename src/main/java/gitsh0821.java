034dfa0ab264c24bdebe470d22f54abb2179977e
Wangpengkai <Wangpengkai@atguigu.com>
2021-02-26 20:04:04 +0800

    ssh免密测试

--- src/main/java/gitsh0821.java
+++ src/main/java/gitsh0821.java
@@ -5,5 +5,6 @@ public class gitsh0821 {
         System.out.println("hello master");
         System.out.println("厉害");
         System.out.println("哈哈");
+        System.out.println("ssh免密测试");
     }
 }
